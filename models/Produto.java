3fe011f983953d7ef8bf7e76da970458d3c4a460
DricoGrosch <04175116982@edu.udesc.br>
2021-04-04 20:54:36 -0300

    Auto commit 2021-04-04 20:04 by DricoGrosch

---
+++ models/Produto.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+
+/**
+ *
+ * @author Drico
+ */
+@Entity
+public class Produto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String descricao;
+    private long valor;
+    private long qtdEstoque;
+    
+    @OneToOne
+    @JoinColumn(name="categoria")
+    private Categoria categoria;
+
+    public Produto(String descricao, long valor, long qtdEstoque, Categoria categoria) {
+        this.descricao = descricao;
+        this.valor = valor;
+        this.qtdEstoque = qtdEstoque;
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "id=" + id + ", descricao=" + descricao + ", valor=" + valor + ", qtdEstoque=" + qtdEstoque + ", categoria=" + categoria + '}';
+    }
+
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Produto() {
+    }
+    
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Produto(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Produto)) {
+            return false;
+        }
+        Produto other = (Produto) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+}
